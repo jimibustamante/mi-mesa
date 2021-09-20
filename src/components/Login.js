02a9cb809395baa150e9a1def8a3418c73ba870d
Jaime Bustamante <jimibustamante@gmail.com>
2021-09-20 20:42:42 -0300

    Fix Login view

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,6 @@
 import React, { useEffect, memo } from 'react'
 import firebaseApp, { loginOptions } from '../lib/firebaseApp'
 import * as firebaseui from 'firebaseui/dist/esm__es_419'
-import 'firebase/auth'
 
 import 'firebaseui/dist/firebaseui.css'
 import '../styles/Login.scss'
@@ -10,13 +9,13 @@ function Login() {
   useEffect(() => {
     try {
       if (!firebaseApp) return
-      const ui = new firebaseui.auth.AuthUI(firebaseApp.auth())
+      const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebaseApp.auth())
       ui.start('#firebase-login', loginOptions)
     } catch (error) {
       console.error({error})
     }
 
-  }, [firebaseApp])
+  }, [firebaseApp, firebaseui])
 
   return (
     <div className="login-container container text-center">
